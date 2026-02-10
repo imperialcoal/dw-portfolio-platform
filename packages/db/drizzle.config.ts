31be0069cd48f1efb0745b29d85a3ed7579fcc8f
imperialcoal <programmer.software.dwarren@gmail.com>
2026-02-09 22:16:23 -0500

    fix: rever drizzle config to use relative paths and add with-env flags locally to scripts

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,11 +1,10 @@
-import { resolve } from "node:path";
 import type { Config } from "drizzle-kit";
 
 import { config } from "@dw/config";
 
 export default {
-  schema: resolve(import.meta.dirname, "./src/schema.ts"),
-  out: resolve(import.meta.dirname, "./drizzle"),
+  schema: "./src/schema.ts",
+  out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: { url: config.db.DIRECT_URL },
   casing: "snake_case",
